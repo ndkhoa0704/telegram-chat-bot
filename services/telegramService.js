e6b3283f3467756967bd75582c67e041fac8915e
Khoa Nguyen Dang <ndkhoa0704@gmail.com>
2025-10-17 14:26:46 +0700

    Refactor TelegramService to improve command handling and message sending functionality. Introduce a dedicated sendMessage method for sending messages to Telegram, and update command handlers to utilize this method. Simplify response handling in the webhook processing logic for better clarity and error management.

--- services/telegramService.js
+++ services/telegramService.js
@@ -4,45 +4,64 @@ const PostgresService = require('./databaseService');
 
 function TelegramService() {
     const SELF = {
-        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-        TELEGRAM_API_URL: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`,
-        TELEGRAM_WEBHOOK_URL: `https://${process.env.TELEGRAM_WEBHOOK_URL}/api/webhook`,
+        BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+        API_URL: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`,
+        WEBHOOK_URL: `https://${process.env.TELEGRAM_WEBHOOK_URL}/api/webhook`,
+        SEND_MSG_URL: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+        sendMessage: async (msg, chatId) => {
+            const postData = {
+                chat_id: chatId,
+                text: msg,
+                parse_mode: 'markdown'
+            };
+
+            const response = await fetch(SELF.SEND_MSG_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(postData)
+            });
+            return response;
+        },
         COMMANDS: {
-            '/tasks': async (_msg) => {
+            '/tasks': async (req, res) => {
                 try {
                     const tasks = await PostgresService.executeQuery(`
                         select cron, prompt, name
                         from tasks
                     `)
                     const msg = tasks.map(task => `- ${task.cron} - ${task.name}`).join('\n');
-                    return msg
-
+                    const response = await SELF.sendMessage(msg, req.body.message.chat.id);
+                    if (!response.ok) return res.status(500).json({ error: response.error });
+                    return res.status(200).json({ status: 'ok' });
                 } catch (error) {
                     console.error(error);
+                    return res.status(500).json({ error: error.message });
                 }
-
             },
-            '/createtask': async msg => {
+            '/createtask': async (req, res) => {
                 try {
                     if (!content) {
-                        return 'Usage: /createtask <cron> - <prompt>';
+                        return 'Usage: /createtask';
                     }
                     const [cron, prompt] = content.split(' - ');
                     const description = await LmService.getResponse(`
-                        Summarize the given AI prompt into a concise description (≤100 characters) that captures its main intent.
-
+                        Summarize the given AI prompt into a concise description (≤100 characters) that captures its main intent:\
                         ${prompt}
-                        `);
+                    `);
 
-                    
                     await PostgresService.executeQuery(`
                         insert into tasks (cron, prompt, description)
                         values ($1, $2, $3)
                     `, [cron, prompt, description]);
 
-                    return `Task created successfully`;
+                    const response = await SELF.sendMessage(`Task created successfully`, req.body.message.chat.id);
+                    if (!response.ok) return res.status(500).json({ error: response.error });
+                    return res.status(200).json({ status: 'ok' });
                 } catch (error) {
                     console.error(error);
+                    return res.status(500).json({ error: error.message });
                 }
             }
         }
@@ -50,12 +69,12 @@ function TelegramService() {
 
     return {
         setupWebhook: async () => {
-            const response = await fetch(`${SELF.TELEGRAM_API_URL}/setWebhook?url=${SELF.TELEGRAM_WEBHOOK_URL}`);
+            const response = await fetch(`${SELF.API_URL}/setWebhook?url=${SELF.WEBHOOK_URL}`);
             const data = await response.json();
             console.log(data);
         },
         deleteWebhook: async () => {
-            const response = await fetch(`${SELF.TELEGRAM_API_URL}/deleteWebhook`);
+            const response = await fetch(`${SELF.API_URL}/deleteWebhook`);
             const data = await response.json();
             console.log('Delete webhook response:', data);
             return data;
@@ -65,58 +84,18 @@ function TelegramService() {
                 // Extract message data from webhook payload
                 const update = req.body;
 
-                console.log('Received message:', update);
-
-                let replyText = null;
-
                 if (update.message.text.startsWith('/')) {
                     const command = update.message.text.split(' ')[0];
-                    console.log('Command:', command);
                     const commandHandler = SELF.COMMANDS[command];
-                    if (commandHandler) {
-                        replyText = await commandHandler(update.message);
-                    } else {
-                        replyText = 'Command not found';
-                    }
+                    if (commandHandler) return commandHandler(req, res);
+                    else return res.status(200).json({ status: 'ok' });
                 }
+                return res.status(200).json({ status: 'ok' });
 
-                if (!replyText) {
-                    return res.status(200).json({ status: 'ok' });
-                }
-
-                const postData = {
-                    chat_id: update.message.chat.id,
-                    text: replyText,
-                    parse_mode: 'markdown'
-                };
-
-                const response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(postData)
-                });
-
-                const data = await response.json();
-
-                if (response.ok) {
-                    console.log('Reply sent successfully');
-                    res.status(200).json({ status: 'ok' });
-                } else {
-                    console.error('Failed to send reply:', data);
-                    res.status(500).json({ error: 'Failed to send reply' });
-                }
             } catch (error) {
                 console.error('Error processing webhook:', error);
                 res.status(500).json({ error: 'Internal server error' });
             }
-        },
-        sendMessage: async (chatId, message) => {
-            const response = await fetch(`${SELF.TELEGRAM_API_URL}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`);
-            const data = await response.json();
-            console.log(data);
-            return data;
         },
     }
 }
